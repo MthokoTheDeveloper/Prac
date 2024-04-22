5d807d6544fda3a4eed5e9dbb773641243db99a2
MthokoTheDeveloper <moyomart01@gmail.com>
2024-04-22 18:08:42 +0200

    Initial commit

---
+++ app/editTopic/[id]/page.jsx
@@ -0,0 +1,10 @@
+import EditTopicForm from "@/components/EditTopicForm";
+
+export default function EditTopic() {
+    return (
+        <div>
+            {/* <EditTopicForm /> */}
+            edit topic page
+        </div>
+    );
+}
